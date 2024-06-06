10f9e9fe3ce364650635f3314b692c7ebc49cb73
Vinicius Rodrigues da Silva <vinicius.rodrigues2004@gmail.com>
2024-06-05 22:10:24 -0300

    Requisição realizada com os filtros e objeto de exibição na tela montado e exibido

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,20 +16,43 @@ export class AppComponent {
 
   condicoesAmbientais: Array<any> = [];
 
+  linhasTable: Array<any> = [];
+
   filtros = {
     regiao: '',
-    especies: '',
+    especie: '',
     statusConservacao: '',
     temperaturaAgua: '',
     PH: '',
-    nivelPoluicao: ''
+    nivelPoluicao: '',
+    pagina: 1
   }
 
   search() {
-    this.http.get('https://fiap-3sis-gs-20241.azurewebsites.net/OceanData?pagina=1&qtde=5').subscribe((condicoesApi: any) => {
-      console.log(condicoesApi)
+    this.linhasTable = [];
+
+    this.http.get(`https://fiap-3sis-gs-20241.azurewebsites.net/OceanData?regiao=${this.filtros.regiao}&especie=${this.filtros.especie}&statusConservacao=${this.filtros.statusConservacao}&temperaturaMin=${this.filtros.temperaturaAgua}&temperaturaMax=${this.filtros.temperaturaAgua}&phMin=${this.filtros.PH}&phMax=${this.filtros.PH}&nivelPoluicao=${this.filtros.nivelPoluicao}&pagina=${this.filtros.pagina}&qtde=3`).subscribe((condicoesApi: any) => {
+
+      condicoesApi.forEach((condicao: any) => {
+        condicao.especies.forEach((condicaoEspecies: any) => {
+
+          this.linhasTable.push({
+            regiao: condicao.regiao,
+            temperatura: condicao.temperaturaAgua,
+            ph: condicao.pH,
+            nivelPoluicao: condicao.nivelPoluicao,
+            especieNome: condicaoEspecies.nome,
+            especieStatus: condicaoEspecies.status,
+          });
+
+
+        })
+      })
+      console.log(this.linhasTable);
+
     })
   }
+
 
   log(teste: any) {
     console.log(teste.target.value)
