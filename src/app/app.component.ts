3572d71b87cc42a2d73b13e55b86b712dd0f3801
Kaua vieira da silva <thiago_viera116@hotmail.com>
2024-06-05 21:21:09 -0300

    Filtros configurados e requisição à api realizada.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'GS01-Matamba';
+  constructor(private http: HttpClient) {}
+
+  condicoesAmbientais: Array<any> = [];
+
+  filtros = {
+    regiao: '',
+    especies: '',
+    statusConservacao: '',
+    temperaturaAgua: '',
+    PH: '',
+    nivelPoluicao: ''
+  }
+
+  search() {
+    this.http.get('https://fiap-3sis-gs-20241.azurewebsites.net/OceanData?pagina=1&qtde=5').subscribe((condicoesApi: any) => {
+      console.log(condicoesApi)
+    })
+  }
+
+  log(teste: any) {
+    console.log(teste.target.value)
+  }
+
+
 }
